9dad19062d61671ca921d108b975ca214b0b01a6
Fredrik F. Lindhagen <fredrik.forde.lindhagen@dnb.no>
2023-12-07 07:44:48 +0100

    Solve day 6

--- aoc-2023/src/main/kotlin/io/lindhagen/aoc/aoc2023/day6/Day6.kt
+++ aoc-2023/src/main/kotlin/io/lindhagen/aoc/aoc2023/day6/Day6.kt
@@ -3,8 +3,8 @@ package io.lindhagen.aoc.aoc2023.day6
 import io.lindhagen.aoc.utils.BaseDay
 import io.lindhagen.aoc.utils.ListUtils.multiply
 
-internal object Day6 : BaseDay<Int> {
-  override fun task1(input: String): Int {
+internal object Day6 : BaseDay<Long> {
+  override fun task1(input: String): Long {
     val (timeLine, distanceLine) = input.trim().lines()
 
     val time = extractNumbers(timeLine.replace("Time:", "").trim())
@@ -13,18 +13,30 @@ internal object Day6 : BaseDay<Int> {
     val winningTimes = time
       .zip(distance)
       .map { findWinningTimes(it.first, it.second) }
-    return winningTimes.map { it.size }.multiply()
+    return winningTimes.map { it.size }.multiply().toLong()
   }
 
-  override fun task2(input: String): Int {
-    TODO("Not yet implemented")
+  override fun task2(input: String): Long {
+    val (timeLine, distanceLine) = input.trim().lines()
+
+    val time = extractNumbers(timeLine.replace("Time:", "").trim())
+      .joinToString("") { it.toString() }.toLong()
+    val distance = extractNumbers(distanceLine.replace("Distance:", "").trim())
+      .joinToString("") { it.toString() }.toLong()
+
+    println(time)
+    println(distance)
+
+    val winningTimes = findWinningTimes(time, distance)
+
+    return winningTimes.size.toLong()
   }
 
-  private fun extractNumbers(line: String): List<Int> {
-    return line.split("\\W+".toRegex()).map { it.toInt() }
+  private fun extractNumbers(line: String): List<Long> {
+    return line.split("\\W+".toRegex()).map { it.toLong() }
   }
 
-  private fun findWinningTimes(maxTime: Int, winningDistance: Int): List<Pair<Int, Int>> {
+  private fun findWinningTimes(maxTime: Long, winningDistance: Long): List<Pair<Long, Long>> {
     return (0..maxTime).mapNotNull { chargeUpTime ->
       val remainingTime = maxTime - chargeUpTime
       if (remainingTime <= 0) {
