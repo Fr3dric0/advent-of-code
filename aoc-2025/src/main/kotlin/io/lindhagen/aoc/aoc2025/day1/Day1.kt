2e6da9e03c9be08ec5d1b97aa0e91da83b4dbcd4
Fredrik F. Lindhagen <fredrik.forde.lindhagen@dnb.no>
2025-12-01 16:16:59 +0100

    Solve day1 task1

--- aoc-2025/src/main/kotlin/io/lindhagen/aoc/aoc2025/day1/Day1.kt
+++ aoc-2025/src/main/kotlin/io/lindhagen/aoc/aoc2025/day1/Day1.kt
@@ -7,8 +7,38 @@ import io.lindhagen.aoc.utils.BaseDay
  * */
 internal object Day1 : BaseDay<Int> {
   override fun task1(input: String): Int {
+    val initialPosition = 50
+
+    val movement = input
+      .trim()
+      .split("\n")
+      .map { it.trim() }
+      .map { it.first() to it.substring(1) }
+      .map { it.first to it.second.toInt() }
+
+    val intermediaryPositions = mutableListOf<Int>(initialPosition)
+    var currentPosition = initialPosition
+
+    movement.forEach { (direction, distance) ->
+      val difference = when (direction.lowercase()) {
+        "l" -> currentPosition - distance
+        "r" -> currentPosition + distance
+        else -> error("Unknown direction $direction")
+      }
+
+      val newPosition = difference.mod(100)
+
+      println("Position: $currentPosition, New position: $newPosition, direction: $direction, distance: $distance, difference: $difference")
+
+      intermediaryPositions.add(newPosition)
+      currentPosition = newPosition
+    }
+
+    val answer = intermediaryPositions.count { it == 0 }
+    return answer
   }
 
   override fun task2(input: String): Int {
+    return 0
   }
 }
--- aoc-2025/src/test/kotlin/io/lindhagen/aoc/aoc2025/day1/Day1Test.kt
+++ aoc-2025/src/test/kotlin/io/lindhagen/aoc/aoc2025/day1/Day1Test.kt
@@ -9,13 +9,23 @@ internal class Day1Test : BaseTest<Int> {
 
     override val sample = Task(
       input = """
+      L68
+      L30
+      R48
+      L5
+      R60
+      L55
+      L1
+      L99
+      R14
+      L82
       """.trimIndent(),
-      task1 = 209,
+      task1 = 3,
       task2 = 281,
     )
     override val actual = Task(
       input = "day1",
-      task1 = 52974,
+      task1 = 1036,
       task2 = 53340,
     )
 }
