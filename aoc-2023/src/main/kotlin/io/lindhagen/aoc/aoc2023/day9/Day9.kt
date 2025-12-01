d2d76f68b0b6ea7c0abbf6aa503273ddde0fec49
Fredrik F. Lindhagen <fredrik.forde.lindhagen@dnb.no>
2025-12-01 07:59:54 +0100

    Include incomplete tasks

---
+++ aoc-2023/src/main/kotlin/io/lindhagen/aoc/aoc2023/day9/Day9.kt
@@ -0,0 +1,13 @@
+package io.lindhagen.aoc.aoc2023.day9
+
+import io.lindhagen.aoc.utils.BaseDay
+
+internal object Day9 : BaseDay<Long> {
+  override fun task1(input: String): Long {
+    TODO("Not yet implemented")
+  }
+
+  override fun task2(input: String): Long {
+    TODO("Not yet implemented")
+  }
+}
